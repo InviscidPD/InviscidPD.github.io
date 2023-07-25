f5cbefa17aa67aa90657f78f7b0bd672f5096030
InviscidPD <140419333+InviscidPD@users.noreply.github.com>
2023-07-25 12:55:57 -0700

    Add files via upload

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 var width = $(window).width(); 
 window.onscroll = function(){
 if ((width >= 1000)){
-    if(document.body.scrollTop > 80 || document.documentElement.scrollTop > 80) {
+    if(document.body.scrollTop > 40 || document.documentElement.scrollTop > 40) {
         $("#header").css("background","#fff");
         $("#header").css("color","#000");
         $("#header").css("box-shadow","0px 0px 20px rgba(0,0,0,0.09)");
