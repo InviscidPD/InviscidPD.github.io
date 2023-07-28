d947c3cac6f76b486f585a1ca35b9bc6b5eb3a03
InviscidPD <Contact@inviscidpd.com>
2023-07-28 10:28:19 -0700

    1

--- index.js
+++ index.js
@@ -1,32 +1,40 @@
-
-window.onscroll = function(){
-	var width = $(window).width(); 
-if ((width >= 1)){
-    if(document.body.scrollTop > 40 || document.documentElement.scrollTop > 40) {
-        $("#header").css("background","#fff");
-        $("#header").css("color","#000");
-        $("#header").css("box-shadow","0px 0px 20px rgba(0,0,0,0.09)");
-        $("#header").css("padding","2vh 4vw");
-		$("#logo img").css("width","45px");
-        $("#navigation a").hover(function(){
-            $(this).css("border-bottom","2px solid rgb(255, 44, 90)");
-        },function(){
-            $(this).css("border-bottom","2px solid transparent");
-        });
-    }else{
-        $("#header").css("background","transparent");
-        $("#header").css("color","#fff");
-        $("#header").css("box-shadow","0px 0px 0px rgba(0,0,0,0)");
-        $("#header").css("padding","6vh 4vw");
-		$("#logo img").css("width","90px");
-        $("#navigation a").hover(function(){
-            $(this).css("border-bottom","2px solid #fff");
-        },function(){
-            $(this).css("border-bottom","2px solid transparent");
-        });
+// Attach the scroll event handler to the window
+$(window).on("scroll", function() {
+    // Get the current window width
+    var width = $(window).width();
+  
+    if (width >= 1) {
+        if ($(document).scrollTop() > 40) {
+            $("#header").css("background", "#fff");
+            $("#header").css("color", "#000");
+            $("#header").css("box-shadow", "0px 0px 20px rgba(0,0,0,0.09)");
+            $("#header").css("padding", "1vh 4vw");
+			$("#navigation").css("vertical-align", "middle");
+            $("#logo img").css("width", "10px");
+            $("#logo img").attr("src", "images/Logo/Small.svg"); // Corrected image source change
+            $("#navigation a").hover(function() {
+                $(this).css("border-bottom", "2px solid rgb(255, 44, 90)");
+            }, function() {
+                $(this).css("border-bottom", "2px solid transparent");
+            });
+        } else {
+            $("#header").css("background", "transparent");
+            $("#header").css("color", "#fff");
+            $("#header").css("box-shadow", "0px 0px 0px rgba(0,0,0,0)");
+            $("#header").css("padding", "6vh 4vw");
+			$("#navigation").css("vertical-align", "bottom");
+            $("#logo img").css("width", "90px");
+            $("#logo img").attr("src", "images/Logo/InviscidLogo.svg"); // Corrected image source change
+            $("#navigation a").hover(function() {
+                $(this).css("border-bottom", "2px solid #fff");
+            }, function() {
+                $(this).css("border-bottom", "2px solid transparent");
+            });
+        }
     }
-}
-}
+});
+
+
 
 function magnify(imglink){
     $("#img_here").css("background",`url('${imglink}') center center`);
