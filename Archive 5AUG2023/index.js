12de9a8c4bded9b09bee6916bf253becb21feaf8
InviscidPD <Contact@inviscidpd.com>
2023-08-05 08:19:46 -0700

    1

---
+++ Archive 5AUG2023/index.js
@@ -0,0 +1,73 @@
+// Attach the scroll event handler to the window
+$(window).on("scroll", function() {
+    // Get the current window width
+    var width = $(window).width();
+  
+    if (width > 800) {
+        if ($(document).scrollTop() > 40) {
+
+
+            
+        } else {
+
+
+
+        }
+    }
+
+});
+
+
+
+
+
+function myFunction() {
+  var x = document.getElementById("myTopnav");
+  if (x.className === "topnav") {
+    x.className += " responsive";
+  } else {
+    x.className = "topnav";
+  }
+}
+
+function magnify(imglink){
+    $("#img_here").css("background",`url('${imglink}') center center`);
+    $("#magnify").css("display","flex");
+    $("#magnify").addClass("animated fadeIn");
+    setTimeout(function(){
+        $("#magnify").removeClass("animated fadeIn");
+    },800);
+}
+
+function closemagnify(){
+    $("#magnify").addClass("animated fadeOut");
+    setTimeout(function(){
+        $("#magnify").css("display","none");
+        $("#magnify").removeClass("animated fadeOut");
+        $("#img_here").css("background",`url('') center center`);
+    },800);
+}
+
+setTimeout(function(){
+    $("#loading").addClass("animated fadeOut");
+    setTimeout(function(){
+      $("#loading").removeClass("animated fadeOut");
+      $("#loading").css("display","none");
+    },800);
+},1650);
+
+$(document).ready(function(){
+    $("a").on('click', function(event) {
+      if (this.hash !== "") {
+        event.preventDefault();
+        var hash = this.hash;
+        $('body,html').animate({
+        scrollTop: $(hash).offset().top
+        }, 800, function(){
+        window.location.hash = hash;
+       });
+       } 
+      });
+  });
+  
+  
